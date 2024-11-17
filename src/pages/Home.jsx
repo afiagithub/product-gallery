787bb59260a4e4b0fc073a1085fda8f4e4dbda2d
Afia Rahman <rahmanmou26@gmail.com>
2024-11-17 13:41:43 +0600

    added category and price based filtering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,22 +5,40 @@ import ProductCard from '../components/ProductCard';
 import LoadingSpinner from '../components/LoadingSpinner';
 
 const Home = () => {
+    const [allTechs, setAllTechs] = useState([])
     const [techList, setTechList] = useState([]);
     const { data: products = [], isLoading, refetch } = useQuery({
         queryKey: ['products'],
         queryFn: async () => {
             const res = await axios.get('./fakeproducts.json')
+            setAllTechs(res.data)
             setTechList(res.data);
-        }
+        },
+        refetchOnWindowFocus: false,
     })
 
     const handleSearch = async (e) => {
-        e.preventDefault();
-        const name = e.target.name.value;
-        const newTech = techList.filter(tech => tech.name === name);        
+        e.preventDefault();      
+        const name = e.target.name.value;               
+        const newTech = allTechs.filter(tech => tech.name === name);
         setTechList(newTech);
     }
 
+    const handleCat = async (e) => {        
+        e.preventDefault();
+        const category = e.target.value;
+        const newTech = allTechs.filter(tech => tech.category === category);
+        setTechList(newTech);
+    }
+
+    const handlePrice = (e) => {
+        e.preventDefault();
+        const min_price = e.target.min_price.value;
+        const max_price = e.target.max_price.value;
+        const newTech = allTechs.filter(tech => tech.price >= min_price && tech.price <= max_price);
+        setTechList(newTech);        
+    }
+    
     if (isLoading) {
         return <LoadingSpinner />
     }
@@ -30,17 +48,18 @@ const Home = () => {
             <p className="text-center w-3/5 mx-auto mb-10 lg:mb-20">
                 Join our community and stay connected for exclusive offers, new arrivals, and more.
                 Wear your story with Snowbell.</p>
-            <div className="lg:w-4/5 lg:mx-auto mb-8 text-sm px-10 lg:px-0">
-                <form className="flex flex-col lg:flex-row gap-4 items-center justify-end">
-                    <select className="p-3 text-[#921A40] font-semibold border-2 border-[#921A40] 
+            <div className="mb-8 text-sm px-10 lg:px-0 flex flex-col lg:flex-row items-center justify-center gap-5">
+                <select onChange={handleCat} className="p-3 text-[#921A40] font-semibold border-2 border-[#921A40] 
                 hover:text-[#921A40] hover:bg-transparent hover:border-[#921A40] rounded-lg" name="category">
-                        <option selected disabled>Category</option>
-                        <option value="Shirts">Shirts</option>
-                        <option value="Pants">Pants</option>
-                        <option value="Jackets">Jackets</option>
-                        <option value="Dresses">Dresses</option>
-                        <option value="Shoes">Shoes</option>
-                    </select>
+                    <option selected disabled>Category</option>
+                    <option value="Mobile">Mobile</option>
+                    <option value="Television">Television</option>
+                    <option value="Accessories">Accessories</option>
+                    <option value="Laptop">Laptop</option>
+                    <option value="Camera">Camera</option>
+                    <option value="Wearable">Wearable</option>
+                </select>
+                <form onSubmit={handlePrice} className="flex flex-col lg:flex-row gap-4 items-center justify-end">
                     <div className="flex flex-row gap-5">
                         <input type="text" name="min_price" placeholder="Min. Price"
                             className="input input-bordered w-48 md:w-auto" />
@@ -51,7 +70,7 @@ const Home = () => {
                 hover:border-[#921A40] hover:bg-transparent hover:text-[#921A40]">Filter</button>
                 </form>
             </div>
-            <div className="flex flex-row justify-between items-center mb-10 px-10 lg:px-4">
+            <div className="flex flex-col lg:flex-row justify-between items-center gap-5 mb-10 px-10 lg:px-4">
                 <select className="p-3 text-[#921A40] font-semibold border-2 border-[#921A40] 
                 hover:text-[#921A40] hover:bg-transparent hover:border-[#921A40] rounded-lg">
                     <option selected disabled>Sort By</option>
@@ -65,7 +84,7 @@ const Home = () => {
                 hover:border-[#921A40] hover:bg-transparent hover:text-[#921A40]">Search</button>
                 </form>
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 place-items-center'>
                 {
                     techList.map(tech => <ProductCard key={tech.id} tech={tech} />)
                 }
